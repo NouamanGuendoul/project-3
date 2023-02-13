c35123a83bf4c5299c0244abf4c74796fa24c9f8
Your Name <9019188@student.zadkine.nl>
2023-02-13 14:04:24 +0100

    geschiedenis

---
+++ webpage/geschiedenis.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+ <?php include "menu.html"?>
+<h1>Geschiedenis het Dol.com </h1>
+
+<section>
+    <h2>Dol.com</h2>
+    <p>Het bedrijf is opgericht in 1980 door de oprichter Yassin rahmoeni. <br>
+       Hij begon met een fietsen winkel daar ging hij kinderen helpen met <br>
+       hun lekken banden. Daarna ging hij 2 soorten winkels samenvoegen een fietsenwinkel en een supermarkt  <br> 
+       Daar had hij meer connecties met bedrijfsleden gekregen zelfs met de eigenaar met bol.com <br>
+       Ze waren goeie vrienden geworden en de eigenaar van bol.com was een inspiratie van Yassin geworden <br>
+    
+</p>
+</section>
+
+</body>
+</html>
