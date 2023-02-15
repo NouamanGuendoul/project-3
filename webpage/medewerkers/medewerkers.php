39143f9be453b51813f7d4b37898e8f4840ab536
StaceyPeters01 <9018900@student.zadkine.nl>
2023-02-15 09:50:03 +0100

    medewerkers text

--- webpage/medewerkers/medewerkers.php
+++ webpage/medewerkers/medewerkers.php
@@ -5,8 +5,12 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Medewerkers</title>
+    <link rel="stylesheet" href="../menu.css">
 </head>
 <body>
+
+    <?php include '../menu.html';?>
+
     <section class="medewerker-info">
         <h2>Gian Carlo Jansen</h2>
         <p>
@@ -20,6 +24,22 @@
         <p>
             Gregor is zeer sociaal en is daarom fijn om mee te praten als klant. Hij zoekt snel naar oplossingen voor problemen en is goed in onderhandelen. Hij geeft ons veel ideeën en werkt snel.
             Het is fijn om hem in ons team te hebben en hij zorgt er ook voor dat ons bedrijf een efficiënte werkwijze heeft.
+        </p>
+    </section>
+
+    <section class="medewerker-info">
+        <h2>Nouaman Guendoul</h2>
+        <p>
+            Nouaman is een snel werkende collega. Hij helpt ons met taken die we niet alleen uit kunnen voeren. Wij zijn heel blij met onze efficiënte collega, zijn oplossingen zijn heel effectief en vaak 
+            weten we dan ook gelijk wat we moeten doen als het probleem nog een keer op komt dagen. Als klant is het ook fijn om met hem te werken, vooral omdat hij goed luisterd en gelijk oplossingen weet.
+        </p>
+    </section>
+
+    <section class="medewerker-info">
+        <h2>Stacey Peters</h2>
+        <p>
+            Stacey is een fijne collega. Ze is zeker perfectionistisch en daarom hebben onze producten veel meer detail. In ons team is ze een goede aanvulling, ze is rustig en luistert goed. 
+            Zij is minder efficiënt maar met de details die ze in de producten stop mag dat ook wel. Klanten vinden haar fijn en zij begrijpt wat ze willen.
         </p>
     </section>
 </body>
