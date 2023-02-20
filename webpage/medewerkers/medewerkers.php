a61317424f703188d2c19e02f7c3171ef894ae54
StaceyPeters01 <9018900@student.zadkine.nl>
2023-02-20 14:07:58 +0100

    position update

--- webpage/medewerkers/medewerkers.php
+++ webpage/medewerkers/medewerkers.php
@@ -11,37 +11,44 @@
 <body>
 
     <?php include '../menu.html';?>
+ <main id="medewerkers-main">
+    <div id="overons-1">
+            <section id="medewerker-gian">
+                <h2 class="medewerker-name">Gian Carlo Jansen</h2>
+                <p>
+                    Onze eerste medewerker is Gian Carlo Jansen. Hij luistert goed naar de problemen van klanten en is ook nog eens een fijne collega om mee te werken.
+                    Hij heeft veel kennis en weet altijd wat hij moet doen. Oplossingen zoeken voor problemen is een van zijn sterke punten. 
+                </p>
+            </section>
+            
+            <section id="medewerker-gregor">
+                <h2 class="medewerker-name">Gregor Ostrowski</h2>
+                <p>
+                    Gregor is zeer sociaal en is daarom fijn om mee te praten als klant. Hij zoekt snel naar oplossingen voor problemen en is goed in onderhandelen. Hij geeft ons veel ideeën en werkt snel.
+                    Het is fijn om hem in ons team te hebben en hij zorgt er ook voor dat ons bedrijf een efficiënte werkwijze heeft. Hij begrijpt klanten snel.
+                </p>
+            </section>
+        </div>
+        
+        <div id="overons-2">
+            <section id="medewerker-nouaman">
+                <h2 class="medewerker-name">Nouaman Guendoul</h2>
+                <p>
+                    Nouaman is een snel werkende collega. Hij helpt ons met taken die we niet alleen uit kunnen voeren. Wij zijn heel blij met onze efficiënte collega, zijn oplossingen zijn heel effectief en vaak 
+                    weten we dan ook gelijk wat we moeten doen als het probleem nog een keer op komt dagen. Als klant is het ook fijn om met hem te werken, vooral omdat hij goed luisterd en gelijk oplossingen weet.
+                </p>
+            </section>
 
-    <section id="medewerker-gian">
-        <h2 class="medewerker-name">Gian Carlo Jansen</h2>
-        <p>
-            Onze eerste medewerker is Gian Carlo Jansen. Hij luistert goed naar de problemen van klanten en is ook nog eens een fijne collega om mee te werken.
-            Hij heeft veel kennis en weet altijd wat hij moet doen. Oplossingen zoeken voor problemen is een van zijn sterke punten. 
-        </p>
-    </section>
+            <section id="medewerker-stacey">
+                <h2 class="medewerker-name">Stacey Peters</h2>
+                <p>
+                    Stacey is een fijne collega. Ze is zeker perfectionistisch en daarom hebben onze producten veel meer detail. In ons team is ze een goede aanvulling, ze is rustig en luistert goed. 
+                    Zij is minder efficiënt maar met de details die ze in de producten stop mag dat ook wel. Klanten vinden haar fijn en zij begrijpt wat ze willen.
+                </p>
+            </section>
+        </div>
+ </main>
+   
     
-    <section id="medewerker-gregor">
-        <h2 class="medewerker-name">Gregor Ostrowski</h2>
-        <p>
-            Gregor is zeer sociaal en is daarom fijn om mee te praten als klant. Hij zoekt snel naar oplossingen voor problemen en is goed in onderhandelen. Hij geeft ons veel ideeën en werkt snel.
-            Het is fijn om hem in ons team te hebben en hij zorgt er ook voor dat ons bedrijf een efficiënte werkwijze heeft. Hij begrijpt klanten snel.
-        </p>
-    </section>
-
-    <section id="medewerker-nouaman">
-        <h2 class="medewerker-name">Nouaman Guendoul</h2>
-        <p>
-            Nouaman is een snel werkende collega. Hij helpt ons met taken die we niet alleen uit kunnen voeren. Wij zijn heel blij met onze efficiënte collega, zijn oplossingen zijn heel effectief en vaak 
-            weten we dan ook gelijk wat we moeten doen als het probleem nog een keer op komt dagen. Als klant is het ook fijn om met hem te werken, vooral omdat hij goed luisterd en gelijk oplossingen weet.
-        </p>
-    </section>
-
-    <section id="medewerker-stacey">
-        <h2 class="medewerker-name">Stacey Peters</h2>
-        <p>
-            Stacey is een fijne collega. Ze is zeker perfectionistisch en daarom hebben onze producten veel meer detail. In ons team is ze een goede aanvulling, ze is rustig en luistert goed. 
-            Zij is minder efficiënt maar met de details die ze in de producten stop mag dat ook wel. Klanten vinden haar fijn en zij begrijpt wat ze willen.
-        </p>
-    </section>
 </body>
 </html>
