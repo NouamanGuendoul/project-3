41fec6109d3fc1187023b68d92aed30cb0aab9a3
StaceyPeters01 <9018900@student.zadkine.nl>
2023-02-24 15:30:07 +0100

    i give up

--- webpage/klachten/klachten.php
+++ webpage/klachten/klachten.php
@@ -7,15 +7,15 @@
 
     try{
         $stmt = $conn->prepare("INSERT INTO klachten (naam, bericht, datum)
-        VALUES (:naam, :bericht, :datum)");
+        VALUES (:naam, :klacht, :datum)");
         $stmt->bindParam('naam',$naam);
-        $stmt->bindParam('bericht',$bericht);
+        $stmt->bindParam('klacht',$bericht);
         $stmt->bindParam('datum',$datum);
 
         if(isset($_REQUEST['naam'])){
 
             $naam = $_POST['naam'];
-            $bericht = $_POST['bericht'];
+            $bericht = $_POST['klacht'];
             $datum = Date('d-m-Y');
             $stmt->execute();
 
@@ -26,8 +26,8 @@
     catch(PDOException $e){
         echo "Error!: " . $e->getMessage();
     }
-    $sqlSelect = "SELECT * FROM klachten";
-    $data = $conn->query($sqlSelect);
+    // $sqlSelect = "SELECT * FROM klachten";
+    // $data = $conn->query($sqlSelect);
 
     $conn = null;
 
@@ -35,9 +35,10 @@
 
 
 <!-- Klachten Form -->
-
+<div id="klacht-container">
 <form method="post" action="" id="klacht-form">
     Naam: <br><input type="text" name="naam" id="naam-input"></input><br><br>
     Bericht: <br><textarea type="text" name="klacht" id="klacht-input"></textarea><br><br><br>
     <input type="submit" name="knop" id="knop">
 </form>
+</div>
