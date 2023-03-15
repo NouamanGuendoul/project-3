cd764b83948b0040b7e9436693afe7668f0f902d
StaceyPeters01 <9018900@student.zadkine.nl>
2023-03-15 11:18:37 +0100

    producten fix

--- webpage/producten/producten.php
+++ webpage/producten/producten.php
@@ -1,3 +1,6 @@
+<?php
+include "../navbar/menu.html";
+?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -6,12 +9,14 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="main.css">
+    <link rel="stylesheet" href="./producten.css">
+    <link rel="stylesheet" href="../navbar/menu.css?v=<?php echo time(); ?>">
 </head>
 <body>
-
+    
   <h1 class="pro1"> Alle product</h1>
-
+    <?php
+    
     echo" <div class='product1'> ";
     try {
  
@@ -22,19 +27,19 @@
         $result = $query->fetchAll(PDO::FETCH_ASSOC);
         echo"producten";
         echo '<table border="1" width="200px" height= "50px">' ;
-
+        
         foreach ($result as $data) {
             echo " <tr> <td>  ";
             echo" cijfer :   " . $data['productnaam'] . "<br>";
             echo " naam: " . $data['prijs'] . "";
-
-
+          
+           
         }
         echo "</td> </tr>  </table>";
     }  catch(PDOException $e)  {
         //die("Error!: " . $e->message());
     }
-
+    
     echo"</div> ";
     echo" <div class='product2'> ";
     try {
@@ -47,13 +52,13 @@
         echo"leveranciers";
         echo '<table border="1" width="200px" height= "50px">
         ' ;
-
+        
         foreach ($result as $data) {
             echo " <tr> <td>  ";
             echo" adresgegevensleveranciers : ". $data['adresgegevensleveranciers'] . "<br>";
             echo " volgnummer : " . $data['volgnummer'] . "";
-
-
+          
+           
         }
         echo "</td> </tr>  </table>";
     }  catch(PDOException $e)  {
@@ -67,6 +72,9 @@
 
 
 
-
+    
 </body>
 </html>
+
+
+
