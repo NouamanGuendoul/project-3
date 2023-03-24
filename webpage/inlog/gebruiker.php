add46722f8ad8414d06135723b5a5dd015b198f5
Your Name <9019188@student.zadkine.nl>
2023-03-24 17:41:38 +0100

    login fix

--- webpage/inlog/gebruiker.php
+++ webpage/inlog/gebruiker.php
@@ -1,6 +1,10 @@
-<?php include 'menu.php';
+<link rel="stylesheet" href="../navbar/menu.css?v=<?php echo time(); ?>">
+
+
+<?php include '../navbar/menu.html';
 $naam=$_POST['Naam'];
 $wachtwoord=$_POST['Wachtwoord'];
+ $db = new PDO("mysql:host=localhost;dbname=klanten", "root", "");
 $query = $db->prepare("INSERT INTO klant(Naam ,wachtwoord)
 VALUES(:naam , :wachtwoord  ) ");
 $query->bindparam("naam" , $naam);
