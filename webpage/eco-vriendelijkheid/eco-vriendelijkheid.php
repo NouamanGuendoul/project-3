71bf92113b58327f845a6159415cb26434ade675
StaceyPeters01 <9018900@student.zadkine.nl>
2023-03-27 13:33:17 +0200

    eco css

--- webpage/eco-vriendelijkheid/eco-vriendelijkheid.php
+++ webpage/eco-vriendelijkheid/eco-vriendelijkheid.php
@@ -7,20 +7,24 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>eco-vriendelijkheid</title>
+    <link rel="stylesheet" href="eco-vriendelijkheid.css">
     <link rel="stylesheet" href="../navbar/menu.css?v=<?php echo time(); ?>">
 
 </head>
 <body>
-    <section>
-    <h2> Eco Vriendelijkheid</h2>
-    <p> Om ervoor te zorgen dat wij eco-vriendelijk zijn zorgen wij ervoor <br> dat wij geen factuur meesturen met de 
-    producten maar dat wij de factuur digitaal <br> via e-mail sturen zodat wij papier besparen. <br>Ook zorgen wij ervoor dat wij 
-    millieuvriendelijke opvulmateriaal <br> gebruiken in plaats van plastic.</p>
-    <br>
-    <p>Wij gebruiken ook elektrische auto's om te bezorgen zodat wij minder Co2 uitsoten. 
-    <br> Wij gebruiken ook in onze werkplek papieren bekers in plaats van plastic bekers. 
-    <br> In plaats van gas gebruiken wij zonnepannelen om onze werkplek te verwarmen en ook gebruiken wij windenergie voor elektriciteit</p>
+<div class="eco-vriendelijkheidgregor">
+        <section class="ecogregor" > 
 
-    </section>
+            <h2 class="comgregor">Eco-vriendelijkheid</h2>
+            <p> Om ervoor te zorgen dat wij eco-vriendelijk zijn zorgen wij ervoor <br> dat wij geen factuur meesturen met de 
+                producten maar dat wij de factuur digitaal <br> via e-mail sturen zodat wij papier besparen. <br>Ook zorgen wij ervoor dat wij 
+                millieuvriendelijke opvulmateriaal <br> gebruiken in plaats van plastic.</p>
+            <br>
+            <p> Wij gebruiken ook elektrische auto's om te bezorgen zodat wij minder Co2 uitsoten. 
+            <br>Wij gebruiken ook in onze werkplek papieren bekers in plaats van plastic bekers. 
+            <br>In plaats van gas gebruiken wij zonnepannelen om onze werkplek te verwarmen en ook gebruiken wij windenergie voor elektriciteit</p>
+
+        </section>
+    </div>
 </body>
 </html>
